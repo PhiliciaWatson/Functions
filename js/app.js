24caa96d4c077a874b68dcff4ba01ae69a5213cc
PhiliciaWatson <philiciawatson7@gmail.com>
2020-07-09 11:02:43 -0400

    Submit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
-var Ice = 4;
-var Cream = 6;
+var ice = 4;
+var cream = 6;
 
-function Shop(){
-    
+function add(argument1,argument2){
+return argument1 + argument2;
 }
-Shop(1,3);
+console.log(add(ice,cream))
+
+ function subtract(argument1, argument2){
+     return argument1 - argument2;
+ }
+ console.log(subtract(ice,cream))
+
+ function multiply(val1, val2){
+     return val1 * val2;
+ }
+ console.log(multiply(ice,cream))
+ 
+ function divide(val1,val2){
+     return val1 / val2;
+ }
+ console.log(divide(ice,cream))
+
+
+var B = [5,6,7,8]
+var C = [9,10,11,12]
+
+function alphabet(twinkle){
+    var A = [1,2,3,4];
+    A.push(7);
+}
